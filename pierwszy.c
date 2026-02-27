8cb2d671453ced509849bbe68c4a856ad1ae2467
Nadia Cichocka <nadcic1@st.amu.edu.pl>
2026-02-27 10:59:32 +0100

    Added name, note and math problem

---
+++ pierwszy.c
@@ -0,0 +1,11 @@
+#include <iostream> 
+
+int main() {  
+    std::cout << "First time using C lang" << std::endl;  
+
+    std::cout << "My name is " << "Nadia" << std::endl; 
+    std::cout << "6 + 7 = " << (6 + 7) << std::endl; 
+
+    return 0;
+}
+
