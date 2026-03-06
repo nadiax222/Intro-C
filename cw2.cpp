2e88883cc6301519287b60ff606c718a33982fe8
Nadia Cichocka <nadcic1@st.amu.edu.pl>
2026-03-06 10:29:28 +0100

    Removed unnecessary folders and added new excercises

---
+++ cw2.cpp
@@ -0,0 +1,38 @@
+#include <iostream> 
+int main() {
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
+
+int main() {
+    int a = 7;
+    float b = 7.5;
+    char letter = 'A';
+}
+
+int main() {
+    int a = 3;
+    int b = 11;
+    int sum = a + b;
+    std::cout << "The sum of " << a << " and " << b << " is: " << sum << std::endl;
+    return 0;
+}
+
+int main() {
+    float x = 12;
+    float y = 7;
+    float division = x / y;
+    std::cout << "Result of division: " << division << std::endl;
+    float x = division;
+    std::cout << "Value of x after assignment: " << x << std::endl;
+    return 0;
+}
+
+int main() {
+    char name[] = "Nadia";
+    char surname[] = "Cichocka";
+    std::cout << "Name: " << name << std::endl;
+    std::cout << "Surname: " << surname << std::endl;
+    return 0;
+}
+
