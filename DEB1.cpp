0488ae39a277ffc32dc5289b1c7871ddd908c46d
Nadia Cichocka <nadcic1@st.amu.edu.pl>
2026-04-24 12:50:56 +0200

    added new ex

---
+++ DEB1.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+
+int main() {
+	double l1, l2;
+	int n;
+
+	std::cin >> l1;
+	std::cin >> l2;
+
+	std::cin >> n;
+    // Wczytanie i przetwarzanie w pętli dopóki dostępne są dane wejściowe
+	for (int i = 1; i < n;) {
+		double wynik1 = l1 * 2.0;
+		double wynik2 = l2 * 3.0;
+
+		std::cout << "Pierwsza * 2 = " << wynik1 << std::endl;
+		std::cout << "Druga * 3  = " << wynik2 << std::endl;
+
+		std::cout << "iteracja nr: " << i << std::endl;
+
+		++i;
+		}	
+
+
+	return 0;
+}
+
+
