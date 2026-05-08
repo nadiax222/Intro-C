6a545a93c238be4887b1eb096d23d3551940b28e
Nadia Cichocka <nadcic1@st.amu.edu.pl>
2026-05-08 12:51:29 +0200

    added excercises

--- 0805.cpp
+++ 0805.cpp
@@ -1,30 +1,50 @@
 ﻿//zad.3 08.05
 #include <iostream>
+//int main()
+//{
+//    int a = 10;
+//    int b = 20;
+//
+//    int* wskaznik = &a;
+//
+//    std::cout << "Adres zmiennej a: " << &a << std::endl;
+//    std::cout << "Adres zmiennej b: " << &b << std::endl;
+//    std::cout << "Wartosc wskaznika: " << wskaznik << std::endl;
+//
+//    int* wskaznik_nowy = nullptr;
+//
+//    std::cout << "Nowy wskaznik: "
+//        << wskaznik_nowy << std::endl;
+//
+//    wskaznik_nowy = &b;
+//
+//    std::cout << "wskaznik_nowy: "
+//        << wskaznik_nowy << std::endl;
+//
+//    std::cout << "Wartosc pod wskaznikiem: "
+//        << *wskaznik_nowy << std::endl;
+//
+//    return 0;
+//}
+
+//tablice
+
+//zad1
+#include <iostream>
 int main()
 {
-    int a = 10;
-    int b = 20;
+	int liczby[5] = { 2, 3, 7, 9, 13 };
+	std::cout << "Elementy tablicy: " << std::endl;
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << liczby[i] << std::endl;
+	}
+	std::cout << "Adres pierwszego elementu: "
+		<< &liczby[0] << std::endl;
+	std::cout << "Adres calej tablicy: "
+		<< liczby << std::endl;
 
-    int* wskaznik = &a;
-
-    std::cout << "Adres zmiennej a: " << &a << std::endl;
-    std::cout << "Adres zmiennej b: " << &b << std::endl;
-    std::cout << "Wartosc wskaznika: " << wskaznik << std::endl;
-
-    int* wskaznik_nowy = nullptr;
-
-    std::cout << "Nowy wskaznik: "
-        << wskaznik_nowy << std::endl;
-
-    wskaznik_nowy = &b;
-
-    std::cout << "wskaznik_nowy: "
-        << wskaznik_nowy << std::endl;
-
-    std::cout << "Wartosc pod wskaznikiem: "
-        << *wskaznik_nowy << std::endl;
-
-    return 0;
+	return 0;
 }
 
-
+//zad2
