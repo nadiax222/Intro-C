f8c0fb1f9dcc2c398ab8fd046bad80c998cfb8f2
Nadia Cichocka <nadcic1@st.amu.edu.pl>
2026-05-08 12:52:18 +0200

    //

--- 0805.cpp
+++ 0805.cpp
@@ -1,5 +1,5 @@
 ﻿//zad.3 08.05
-#include <iostream>
+//#include <iostream>
 //int main()
 //{
 //    int a = 10;
@@ -30,21 +30,40 @@
 //tablice
 
 //zad1
-#include <iostream>
-int main()
-{
-	int liczby[5] = { 2, 3, 7, 9, 13 };
-	std::cout << "Elementy tablicy: " << std::endl;
-	for (int i = 0; i < 5; i++)
-	{
-		std::cout << liczby[i] << std::endl;
-	}
-	std::cout << "Adres pierwszego elementu: "
-		<< &liczby[0] << std::endl;
-	std::cout << "Adres calej tablicy: "
-		<< liczby << std::endl;
-
-	return 0;
-}
+//#include <iostream>
+//int main()
+//{
+//	int liczby[5] = { 2, 3, 7, 9, 13 };
+//	std::cout << "Elementy tablicy: " << std::endl;
+//	for (int i = 0; i < 5; i++)
+//	{
+//		std::cout << liczby[i] << std::endl;
+//	}
+//	std::cout << "Adres pierwszego elementu: "
+//		<< &liczby[0] << std::endl;
+//	std::cout << "Adres calej tablicy: "
+//		<< liczby << std::endl;
+//
+//	return 0;
+//}
 
 //zad2
+//#include <iostream>
+//int main()
+//{
+//	int potega[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+//	std::cout << "Elementy tablicy: " << std::endl;
+//    for (int i = 0; i < 8; i++)
+//    {
+//        potega[i] = potega[i] * potega[i];
+//    }
+//    std::cout << "Elementy tablicy po potegowaniu:" << std::endl;
+//
+//    for (int i = 0; i < 8; i++)
+//    {
+//        std::cout << potega[i] << std::endl;
+//    }
+//
+//    return 0;
+//}
+//
