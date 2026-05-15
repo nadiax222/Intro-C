1076b334e74a0e27e48f0e62ed89cfa8c1b3f8ae
Nadia Cichocka <nadcic1@st.amu.edu.pl>
2026-05-15 12:23:06 +0200

    dded

---
+++ 15052.cpp
@@ -0,0 +1,42 @@
+﻿//zad.5
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	{
+//		char name[100];
+//		cout << "Podaj imię bez spacji i z małych liter:";
+//		cin >> name;
+//		char maxlitera = name[0];
+//
+//		for (int i = 1; name[i] != '\0'; i++) {
+//			if (name[i] > maxlitera) {
+//				maxlitera = name[i];
+//			}
+//		}
+//		cout << "najpozniejsza litera w alfabecie to:"
+//			<< maxlitera << endl;
+//		return 0;
+//
+//	}
+//}
+//
+//
+//zad.6
+#include <iostream>
+using namespace std;
+
+int suma(int tablica[], int rozmiar)
+{
+	int suma = 0;
+	for (int i = 0; i < rozmiar; i++) {
+		suma += tablica[i];
+	}
+	return suma;
+}
+int main()
+{
+	int liczby[] = { 1, 2, 3, 4, 5 };
+	int wynik = suma(liczby, 5);
+	cout << "Suma elementów tablicy wynosi: " << wynik << endl;
+	return 0;
+}
