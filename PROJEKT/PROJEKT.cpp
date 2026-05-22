2321f0933f323cb4044bb36cfb797ec712265045
Nadia Cichocka <nadcic1@st.amu.edu.pl>
2026-05-22 12:42:20 +0200

    added MAIN

---
+++ PROJEKT/PROJEKT.cpp
@@ -0,0 +1,28 @@
+﻿//PROJEKT:
+//- Kod poprawnie wczytuje sekwencje nukleotydów z zaprezentowanego pliku txt.
+//- Kod zamienia DNA na mRNA.
+//- Kod rozpoznaje sekwencje START.
+//- Kod poprawnie zamienia sekwencje nukleotydów między miejscem START a STOP na odpowiednie aminokwasy.
+//- Analiza sekwencji kończy się na sekwencji STOP.
+//- Projekt po zatrzymaniu podaje całą listę wykrytych aminokwasów wraz z ich ilością.
+//Do tego :
+//-Student rozumie swój własny kod i jest w stanie go przedstawić prowadzącemu.
+//- Projekt znajduje się na Githubie studenta.
+//- Projekt wykorzystuje pliki typu.h i.c do deklarowania stałych i funkcji wykorzystywanych w projekcie.
+
+#include <iostream>
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
